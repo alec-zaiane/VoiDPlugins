8f83b4321beb0eb2aade3ca83e66497b0555685c
Alec <50636059+alec-zaiane@users.noreply.github.com>
2024-06-08 21:24:54 -0600

    Add Big movement cutoff to reconstructor

--- src/Filter/Reconstructor/Reconstructor.cs
+++ src/Filter/Reconstructor/Reconstructor.cs
@@ -13,29 +13,30 @@ namespace VoiDPlugins.Filter
         private float weight;
         private float bigMovementCutoff;
 
+        
+
         [Property("EMA Weight"), DefaultPropertyValue(0.5f), ToolTip
         (
             "Default: 0.5\n\n" +
-            "Defines the weight of the latest sample against previous ones [Range: 0.0 - 1.0]\n" +
+            "Defines the weight of the latest sample against previous ones [Range: 0.0 - 1.0] :)\n" +
             "  Lower == More hardware smoothing removed\n" +
             "  1 == No effect"
         )]
-        [Property("Big Movement Cutoff"), DefaultPropertyValue(-1f), ToolTip
-        (
-            "Default: -1\n\n" +
-            "Any move larger than this will not be smoothed, use with relative mode.\n" +
-            "  -1 == No effect\n" +
-        )]
-
         public float EMAWeight
         {
             set => weight = Math.Clamp(value, 0, 1);
             get => weight;
         }
 
+        [Property("Big Movement Cutoff"), DefaultPropertyValue(-1f), ToolTip
+        (
+            "Default: -1\n\n" +
+            "Any move larger than this will not be smoothed, use with relative mode.\n" +
+            "  -1 == No effect"
+        )]
         public float BigMovementCutoff
         {
-            set => bigMovementCutoff = Math.Max(0, value);
+            set => bigMovementCutoff = value == -1 ? value : Math.Max(0, value);
             get => bigMovementCutoff;
         }
 
@@ -47,7 +48,7 @@ namespace VoiDPlugins.Filter
         {
             if (value is ITabletReport report)
             {
-                var truePoint = lastAvg.HasValue ? ReverseEMAFunc(report.Position, lastAvg.Value, (float)EMAWeight) : report.Position;
+                var truePoint = lastAvg.HasValue ? ReverseEMAFunc(report.Position, lastAvg.Value, (float)EMAWeight, (float)BigMovementCutoff) : report.Position;
                 lastAvg = report.Position;
                 report.Position = truePoint;
                 value = report;
@@ -56,7 +57,7 @@ namespace VoiDPlugins.Filter
             Emit?.Invoke(value);
         }
 
-        private static Vector2 ReverseEMAFunc(Vector2 currentEMA, Vector2 lastEMA, float weight)
+        private static Vector2 ReverseEMAFunc(Vector2 currentEMA, Vector2 lastEMA, float weight, float bigMovementCutoff)
         {
             if (bigMovementCutoff != -1 && Vector2.Distance(currentEMA, lastEMA) > bigMovementCutoff)
                 return currentEMA;
